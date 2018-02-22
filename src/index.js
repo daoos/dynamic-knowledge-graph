571e7229b112e6a2a1d07a39f9034bdc2323f989
Marshall Miller <mrmiller@linkedin.com>
2018-02-22 09:27:03 -0800

    Added force-direction with drag and drop. New dataset as well

--- src/index.js
+++ src/index.js
@@ -1,185 +1,155 @@
-import * as d3 from 'd3';
-// dimensions
-var width = 1000;
-var height = 1000;
+var width = 1800,
+    height = 1800;
 
-var margin = {
-    top: 50,
-    bottom: 50,
-    left: 50,
-    right: 50,
-}
 
-// create an svg to draw in
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append('g')
-    .attr('transform', 'translate(' + margin.top + ',' + margin.left + ')');
 
-width = width - margin.left - margin.right;
-height = height - margin.top - margin.bottom;
+d3.csv(("./src/knowledge_graph_final_result.csv"), function (error, links) {
+    var nodes = {};
 
-var simulation = d3.forceSimulation()
-    // pull nodes together based on the links between them
-    .force("link", d3.forceLink().id(function(d) {
-        debugger;
-        return d.id;
-    })
-    .strength(0.025))
-    // push nodes apart to space them out
-    .force("charge", d3.forceManyBody().strength(-200))
-    // add some collision detection so they don't overlap
-    .force("collide", d3.forceCollide().radius(12))
-    // and draw them around the centre of the space
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    // Compute the distinct nodes from the links.
+    links.forEach(function (link) {
+        link.source = nodes[link.source] ||
+            (nodes[link.source] = {
+                name: link.source,
+                courseTitle: link.course_title_x,
+                probability: link.value
+            });
+        link.target = nodes[link.target] ||
+            (nodes[link.target] = {
+                name: link.target,
+                targetCourseTitle: link.target_course_title,
+                probability: link.value
 
-// // // load the graph
-d3.json("./src/data.json", function(error, graph) {
-    // set the nodes
-    var nodes = graph.nodes;
-    // links between nodes
-    var links = graph.links;
+            });
+        link.value = +link.value;
+    });
 
-    // add the curved links to our graphic
-    var link = svg.selectAll(".link")
-        .data(links)
-        .enter()
-          .append("path")
-          .attr("class", "link")
-          .attr('stroke', function(d){
-              return "#ddd";
-          });
 
-    // add the nodes to the graphic
+    var force = d3.layout.force()
+        .nodes(d3.values(nodes))
+        .links(links)
+        .size([width, height])
+        .linkDistance(160)
+        .charge(-1000)
+        .on("tick", tick)
+        .start();
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    // build the arrow.
+    svg.append("svg:defs").selectAll("marker")
+        .data(["end"]) // Different link/path types can be defined here
+        .enter().append("svg:marker") // This section adds in the arrows
+        .attr("id", String)
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 15)
+        .attr("refY", -1.5)
+        .attr("markerWidth", 10)
+        .attr("markerHeight", 10)
+        .attr("orient", "auto")
+        .append("svg:path")
+        .style("fill", "#FFF")
+        .attr("d", "M0,-5L10,0L0,5");
+
+    // add the links and the arrows
+    var path = svg.append("svg:g").selectAll("path")
+        .data(force.links())
+        .enter().append("svg:path")
+        //    .attr("class", function(d) { return "link " + d.type; })
+        .attr("class", "link")
+        .attr("marker-end", "url(#end)");
+
+    // define the nodes
     var node = svg.selectAll(".node")
-        .data(nodes)
+        .data(force.nodes())
         .enter().append("g")
-
-    // a circle to represent the node
-    node.append("circle")
         .attr("class", "node")
+        .style("fill", "#33AADA")
+        .call(force.drag);
+
+    // add the nodes
+    node.append("circle")
         .attr("r", 8)
-        .attr("fill", "#98d8f4")
-// //         .on("mouseover", mouseOver(.2))
-// //         .on("mouseout", mouseOut);
+        .style("stroke",  "#fff")
+        .style("stroke-width", 1.5)
 
-    // hover text for the node
-    node.append("title")
-        .text(function(d) {
-            return d.course_name;
-        });
+    svg.selectAll("circle")
+        .on("mouseover", function(d,i) {
+            var probability = d.probability * 100;
+            d3.selectAll("text")
+                .style("opacity", .1);
 
-    // add a label to each node
-    node.append("text")
-        .attr("dx", 12)
-        .attr("dy", ".35em")
-        .text(function(d) {
-            return d.course_name;
+            d3.select(this.parentNode).append("text")
+            .attr("x", d3.select(this).attr("cx"))
+            .attr("dy", 30)
+            .attr("stroke", "#fff")
+            .attr("stroke-width", ".9")
+            .text(function (d) {
+                return d.targetCourseTitle ? d.targetCourseTitle
+                    : d.courseTitle;
+            });
+
+
+            d3.select(this.parentNode).append("text")
+                .attr("x", d3.select(this).attr("cx"))
+                .attr("dy", 50)
+                .attr("stroke", "#fff")
+                .attr("stroke-width", ".9")
+                .text(function (d) {
+                    debugger;
+                    return d.targetCourseTitle ?
+                        `${Math.round(probability)}% match`
+                        : null;
+                });
+
         })
-        .style("stroke", "#f3f6f8")
-        .style("stroke-width", 0.1)
-        .style("fill", function(d) {
-            return "#f3f6f8";
+        .on("mouseout", function(d,i) {
+            d3.selectAll("text")
+                .style("opacity", 1);
+
+            d3.select(this.parentNode).selectAll("text").remove();
+            node.append("text")
+            .attr("x", 12)
+            .attr("dy", ".35em")
+            .attr("stroke", "#fff")
+            .attr("stroke-width", ".9")
+            .text(function (d) {
+                return d.courseTitle;
+            });
+        })
+
+    // add the text
+    var nodeText = node.append("text")
+        .attr("x", 12)
+        .attr("dy", ".35em")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", ".9")
+        .text(function (d) {
+            return d.courseTitle;
         });
 
-    // add the nodes to the simulation and
-    // tell it what to do on each tick
-    simulation
-        .nodes(nodes)
-        .on("tick", ticked);
+    // add the curvy lines
+    function tick() {
+        path.attr("d", function (d) {
+            var dx = d.target.x - d.source.x,
+                dy = d.target.y - d.source.y,
+                dr = Math.sqrt(dx * dx + dy * dy);
+            return "M" +
+                d.source.x + "," +
+                d.source.y + "A" +
+                dr + "," + dr + " 0 0,1 " +
+                d.target.x + "," +
+                d.target.y;
+        });
 
-    // add the links to the simulation
-    simulation
-        .force("link")
-        .links(links);
-
-    // on each tick, update node and link positions
-    function ticked() {
-        link.attr("d", positionLink);
-        node.attr("transform", positionNode);
+        node
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
     }
-
-    // links are drawn as curved paths between nodes,
-    // through the intermediate nodes
-    function positionLink(d) {
-        var offset = 30;
-
-        var midpoint_x = (d.source.x + d.target.x) / 2;
-        var midpoint_y = (d.source.y + d.target.y) / 2;
-
-        var dx = (d.target.x - d.source.x);
-        var dy = (d.target.y - d.source.y);
-
-        var normalise = Math.sqrt((dx * dx) + (dy * dy));
-
-        var offSetX = midpoint_x + offset*(dy/normalise);
-        var offSetY = midpoint_y - offset*(dx/normalise);
-
-        return "M" + d.source.x + "," + d.source.y +
-            "S" + offSetX + "," + offSetY +
-            " " + d.target.x + "," + d.target.y;
-    }
-
-    // move the node based on forces calculations
-    function positionNode(d) {
-        // keep the node within the boundaries of the svg
-        if (d.x < 0) {
-            d.x = 0
-        };
-        if (d.y < 0) {
-            d.y = 0
-        };
-        if (d.x > width) {
-            d.x = width
-        };
-        if (d.y > height) {
-            d.y = height
-        };
-        return "translate(" + d.x + "," + d.y + ")";
-    }
-
-// //     // build a dictionary of nodes that are linked
-// //     var linkedByIndex = {};
-// //     links.forEach(function(d) {
-// //         linkedByIndex[d.source.index + "," + d.target.index] = 1;
-// //     });
-
-// //     // check the dictionary to see if nodes are linked
-// //     function isConnected(a, b) {
-// //         return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index] || a.index == b.index;
-// //     }
-
-// //     // fade nodes on hover
-// //     function mouseOver(opacity) {
-// //         return function(d) {
-// //             // check all other nodes to see if they're connected
-// //             // to this one. if so, keep the opacity at 1, otherwise
-// //             // fade
-// //             node.style("stroke-opacity", function(o) {
-// //                 thisOpacity = isConnected(d, o) ? 1 : opacity;
-// //                 return thisOpacity;
-// //             });
-// //             node.style("fill-opacity", function(o) {
-// //                 thisOpacity = isConnected(d, o) ? 1 : opacity;
-// //                 return thisOpacity;
-// //             });
-// //             // also style link accordingly
-// //             link.style("stroke-opacity", function(o) {
-// //                 return o.source === d || o.target === d ? 1 : opacity;
-// //             });
-// //             link.style("stroke", function(o){
-// //                 return o.source === d || o.target === d ? o.source.colour : "#ddd";
-// //             });
-// //         };
-// //     }
-
-// //     function mouseOut() {
-// //         node.style("stroke-opacity", 1);
-// //         node.style("fill-opacity", 1);
-// //         link.style("stroke-opacity", 1);
-// //         link.style("stroke", "#ddd");
-// //     }
-
 });
+
+
+
