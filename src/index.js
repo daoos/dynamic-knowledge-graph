1286b1bf862c2dbdf250e34f72bb2f04ed4079bb
Marshall Miller <mrmiller@linkedin.com>
2018-02-22 21:10:41 -0800

    Added new data. Paths have opacity based on weight

--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
 var width = 1800,
     height = 1800;
 
+// load the data
+d3.csv("./src/programming_focus_subset.csv", function (error, links) {
 
-
-d3.csv(("./src/knowledge_graph_final_result.csv"), function (error, links) {
     var nodes = {};
 
-    // Compute the distinct nodes from the links.
+    // parse nodes from links
     links.forEach(function (link) {
         link.source = nodes[link.source] ||
             (nodes[link.source] = {
                 name: link.source,
-                courseTitle: link.course_title_x,
-                probability: link.value
+                courseTitle: link.source_course_title,
             });
         link.target = nodes[link.target] ||
             (nodes[link.target] = {
                 name: link.target,
-                targetCourseTitle: link.target_course_title,
+                courseTitle: link.Target_course_title,
                 probability: link.value
-
             });
         link.value = +link.value;
     });
-
 
     var force = d3.layout.force()
         .nodes(d3.values(nodes))
@@ -34,11 +31,34 @@ d3.csv(("./src/knowledge_graph_final_result.csv"), function (error, links) {
         .on("tick", tick)
         .start();
 
+    // Set the range
+    var v = d3.scale.linear().range([0, 100]);
+
+    // Scale the range of the data
+    // Can probably remove
+    v.domain([0, d3.max(links, function (d) {
+        return d.value;
+    })]);
+
+    // asign a type per value to encode opacity
+    // Thanks to Bostock for this!
+    links.forEach(function (link) {
+        if (v(link.value) <= 25) {
+            link.type = "twofive";
+        } else if (v(link.value) <= .50 && v(link.value) > .25) {
+            link.type = "fivezero";
+        } else if (v(link.value) <= .75 && v(link.value) > .50) {
+            link.type = "sevenfive";
+        } else if (v(link.value) <= 1 && v(link.value) > .75) {
+            link.type = "onezerozero";
+        }
+    });
+
     var svg = d3.select("body").append("svg")
         .attr("width", width)
         .attr("height", height);
 
-    // build the arrow.
+    // build the arrow
     svg.append("svg:defs").selectAll("marker")
         .data(["end"]) // Different link/path types can be defined here
         .enter().append("svg:marker") // This section adds in the arrows
@@ -49,16 +69,18 @@ d3.csv(("./src/knowledge_graph_final_result.csv"), function (error, links) {
         .attr("markerWidth", 10)
         .attr("markerHeight", 10)
         .attr("orient", "auto")
+        .style("fill", "#f3f6f8")
         .append("svg:path")
-        .style("fill", "#FFF")
         .attr("d", "M0,-5L10,0L0,5");
 
     // add the links and the arrows
     var path = svg.append("svg:g").selectAll("path")
         .data(force.links())
         .enter().append("svg:path")
-        //    .attr("class", function(d) { return "link " + d.type; })
-        .attr("class", "link")
+        .style("stroke", "#f3f6f8")
+        .attr("class", function (d) {
+            return "link " + d.type;
+        })
         .attr("marker-end", "url(#end)");
 
     // define the nodes
@@ -66,66 +88,20 @@ d3.csv(("./src/knowledge_graph_final_result.csv"), function (error, links) {
         .data(force.nodes())
         .enter().append("g")
         .attr("class", "node")
-        .style("fill", "#33AADA")
+        .on("click", click)
+        .on("dblclick", dblclick)
         .call(force.drag);
 
     // add the nodes
     node.append("circle")
         .attr("r", 8)
-        .style("stroke",  "#fff")
-        .style("stroke-width", 1.5)
-
-    svg.selectAll("circle")
-        .on("mouseover", function(d,i) {
-            var probability = d.probability * 100;
-            d3.selectAll("text")
-                .style("opacity", .1);
-
-            d3.select(this.parentNode).append("text")
-            .attr("x", d3.select(this).attr("cx"))
-            .attr("dy", 30)
-            .attr("stroke", "#fff")
-            .attr("stroke-width", ".9")
-            .text(function (d) {
-                return d.targetCourseTitle ? d.targetCourseTitle
-                    : d.courseTitle;
-            });
-
-
-            d3.select(this.parentNode).append("text")
-                .attr("x", d3.select(this).attr("cx"))
-                .attr("dy", 50)
-                .attr("stroke", "#fff")
-                .attr("stroke-width", ".9")
-                .text(function (d) {
-                    debugger;
-                    return d.targetCourseTitle ?
-                        `${Math.round(probability)}% match`
-                        : null;
-                });
-
-        })
-        .on("mouseout", function(d,i) {
-            d3.selectAll("text")
-                .style("opacity", 1);
-
-            d3.select(this.parentNode).selectAll("text").remove();
-            node.append("text")
-            .attr("x", 12)
-            .attr("dy", ".35em")
-            .attr("stroke", "#fff")
-            .attr("stroke-width", ".9")
-            .text(function (d) {
-                return d.courseTitle;
-            });
-        })
+        .style("fill", "#1d92c1");
 
     // add the text
-    var nodeText = node.append("text")
+    node.append("text")
         .attr("x", 12)
         .attr("dy", ".35em")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", ".9")
+        .style("fill", "#f3f6f8")
         .text(function (d) {
             return d.courseTitle;
         });
@@ -149,7 +125,34 @@ d3.csv(("./src/knowledge_graph_final_result.csv"), function (error, links) {
                 return "translate(" + d.x + "," + d.y + ")";
             });
     }
+
+    // action to take on mouse click
+    function click() {
+        d3.select(this).select("text").transition()
+            .duration(750)
+            .attr("x", 22)
+            .style("fill", "#f3f6f8")
+            .style("stroke", "lightsteelblue")
+            .style("stroke-width", ".5px")
+            .style("font", "20px sans-serif");
+        d3.select(this).select("circle").transition()
+            .duration(750)
+            .attr("r", 16)
+            .style("fill", "#33AADA");
+    }
+
+    // action to take on mouse double click
+    function dblclick() {
+        d3.select(this).select("circle").transition()
+            .duration(750)
+            .attr("r", 6)
+            .style("fill", "#ccc");
+        d3.select(this).select("text").transition()
+            .duration(750)
+            .attr("x", 12)
+            .style("stroke", "none")
+            .style("fill", "#f3f6f8")
+            .style("stroke", "none")
+            .style("font", "10px sans-serif");
+    }
 });
-
-
-
